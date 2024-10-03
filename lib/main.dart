b73518b71db7d43bca8b17ca26263a1a92528773
Aryan <pakhrinp242@gmail.com>
2024-10-03 08:42:23 +0545

    https://github.com/Pakhrin007/flutter

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "fluter App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
